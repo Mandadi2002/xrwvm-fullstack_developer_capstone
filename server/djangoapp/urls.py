7560ce36dde820c9f141b679bd02ba35415a0dbb
Mandadi2002 <https://github.com/Mandadi2002>
2024-07-26 07:22:39 +0530

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,7 +1,7 @@
 # Uncomment the imports before you add the code
 # from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
+from django.conf.urls.static import static # type: ignore
+from django.conf import settings # type: ignore
 # from . import views
 
 app_name = 'djangoapp'
