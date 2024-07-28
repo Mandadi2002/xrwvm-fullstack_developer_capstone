81d3fd8be4fcf72de598c7dd00fac4f72ffa9803
Mandadi2002 <https://github.com/Mandadi2002>
2024-07-28 19:10:01 +0530

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,10 @@ urlpatterns = [
 
     # path for login
      path(route='login', view=views.login_user, name='login'),
-     path('login/', TemplateView.as_view(template_name="index.html")),
+     #path('login/', TemplateView.as_view(template_name="index.html"))
+     #path('registration/', views.registration_request, name='registration'),
     # path for dealer reviews view
-
+     #path('logout/', views.logout_request, name='logout'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
