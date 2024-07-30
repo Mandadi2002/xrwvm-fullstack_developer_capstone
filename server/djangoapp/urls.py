7c37fdcec90061a356f4f18bd44a6c958a882bfd
Mandadi2002 <Mandadi2002>
2024-07-30 16:58:39 +0530

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,13 +7,13 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-     path('registration/', views.registration_request, name='registration'),
+     #path('registration/', views.registration_request, name='registration'),
     # path for login
      path(route='login', view=views.login_user, name='login'),
-     #path('login/', TemplateView.as_view(template_name="index.html"))
+     path('login/', TemplateView.as_view(template_name="index.html")), # type: ignore
     
     # path for dealer reviews view
-     path('logout/', views.logout_request, name='logout'),
+     #path('logout/', views.logout_request, name='logout'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
